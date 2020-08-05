00be5554e8683db3089fcd01c0676f8fa630cbdf
mmalak7 <marcinmalak7@gmail.com>
2020-08-05 20:20:49 +0200

    Updates

---
+++ precache-manifest.350cadcb47c24eaaf53897272f5a138b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e06dd45c384a2637151248f5791b2d",
+    "url": "/%7BGithub-repo-name%7D/index.html"
+  },
+  {
+    "revision": "014209babbc1eccc31d0",
+    "url": "/%7BGithub-repo-name%7D/static/css/main.fc266b0c.chunk.css"
+  },
+  {
+    "revision": "5baf001b86b72193623f",
+    "url": "/%7BGithub-repo-name%7D/static/js/2.7ce431f5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/%7BGithub-repo-name%7D/static/js/2.7ce431f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "014209babbc1eccc31d0",
+    "url": "/%7BGithub-repo-name%7D/static/js/main.c23b9538.chunk.js"
+  },
+  {
+    "revision": "f173f1f1e878fec50fde",
+    "url": "/%7BGithub-repo-name%7D/static/js/runtime-main.7b5a45a2.js"
+  }
+]);
